adb257546f62cbd6553d54c3a5ba0de8c51d8628
nairasaidali <nairasaidali@hotmail.fr>
2015-10-03 12:32:44 +0200

    Commit projet final Fait avec Bilale

--- src/main/java/com/pbz4esilv/gildedrose/Inventory.java
+++ src/main/java/com/pbz4esilv/gildedrose/Inventory.java
@@ -31,8 +31,7 @@ public class Inventory {
                 if (items[i].getQuality() > 0) {
                     if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
                         items[i].setQuality(items[i].getQuality() - 1);
-                        if(items[i].getName() == "Conjured Mana Cake")
-                        {
+                        if (items[i].getName() == "Conjured Mana Cake") {
                             items[i].setQuality(items[i].getQuality() - 1);
                         }
                     }
@@ -41,17 +40,18 @@ public class Inventory {
                 if (items[i].getQuality() < 50) {
                     items[i].setQuality(items[i].getQuality() + 1);
 
-                        if (items[i].getSellIn() < 11) {
-                            if (items[i].getQuality() < 50) {
-                                items[i].setQuality(items[i].getQuality() + 1);
-                            }
+                    if (items[i].getSellIn() < 11) {
+                        if (items[i].getQuality() < 50) {
+                            items[i].setQuality(items[i].getQuality() + 1);
                         }
+                    }
 
-                        if (items[i].getSellIn() < 6) {
-                            if (items[i].getQuality() < 50) {
-                                items[i].setQuality(items[i].getQuality() + 1);
-                            }
+                    if (items[i].getSellIn() < 6) {
+                        if (items[i].getQuality() < 50) {
+                            items[i].setQuality(items[i].getQuality() + 1);
                         }
+                    }
+
                 }
             }
 
@@ -59,44 +59,41 @@ public class Inventory {
                 items[i].setSellIn(items[i].getSellIn() - 1);
             }
 
-            if (items[i].getSellIn() == 0 && items[i].getName() == "Backstage passes to a TAFKAL80ETC concert")
-            {
+            if (items[i].getSellIn() == 0 && items[i].getName() == "Backstage passes to a TAFKAL80ETC concert") {
                 concert_achieved = true;
             }
 
+
             if (items[i].getSellIn() < 0) {
-                    if (items[i].getSellIn() < 0) {
-                        if (items[i].getName() != "Aged Brie" && items[i].getName() != "Backstage passes to a TAFKAL80ETC concert") {
-                                if (items[i].getQuality() > 0) {
-                                    if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
-                                        items[i].setQuality(items[i].getQuality() - 1);
-                                        if(items[i].getName() == "Conjured Mana Cake")
-                                        {
-                                            items[i].setQuality(items[i].getQuality() - 1);
-                                        }
-                                    }
-                            } else {
-                                    if (items[i].getName() == "Backstage passes to a TAFKAL80ETC concert") {
-                                        items[i].setQuality(items[i].getQuality() - items[i].getQuality());
+                if (items[i].getName() != "Aged Brie" && items[i].getName() != "Backstage passes to a TAFKAL80ETC concert") {
 
-                                    }
-                                    if (concert_achieved == true ) {
-
-                                        if( items[i].getName()== "Aged Brie")
-                                        {
-
-                                            items[i].setQuality(items[i].getQuality() - items[i].getQuality());
-                                        }
-
-                                    }
-
+                    if (items[i].getQuality() > 0) {
+                        if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
+                            items[i].setQuality(items[i].getQuality() - 1);
+                            if (items[i].getName() == "Conjured Mana Cake") {
+                                items[i].setQuality(items[i].getQuality() - 1);
                             }
                         }
                     }
+
+                } else {
+                    if (items[i].getName() == "Backstage passes to a TAFKAL80ETC concert") {
+                        items[i].setQuality(items[i].getQuality() - items[i].getQuality());
+
+                    }
+                    if (concert_achieved == true) {
+
+                        if (items[i].getName() == "Aged Brie") {
+
+                            items[i].setQuality(items[i].getQuality() - items[i].getQuality());
+                        }
+
+                    }
+
+
+                }
             }
         }
-
-
     }
 
     public void display()
